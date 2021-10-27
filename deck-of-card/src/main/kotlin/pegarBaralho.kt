c93e0f59ff6f2ce2adb29872c0e009653f6d620b
Kaique-Andrade <kaique_andrade17@hotmail.com>
2021-10-27 15:35:37 -0300

    Estruturação projeto com Kotlin

--- deck-of-card/src/main/kotlin/pegarBaralho.kt
+++ deck-of-card/src/main/kotlin/pegarBaralho.kt
@@ -1,7 +1,12 @@
+import Model.Card
+import controller.DeckController
+import org.json.JSONArray
+import org.json.JSONObject
+
 fun main() {
-    val resposta = khttp.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-    val baralho_id = resposta.jsonObject["deck_id"]
-    val cartas_resposta = khttp.get("https://deckofcardsapi.com/api/deck/$baralho_id/draw/?count=1")
-    val valor_carta = cartas_resposta.jsonObject["cards"]
-    println(valor_carta)
+    val deckController = DeckController()
+    val carta = deckController.drawCard()
+    println(carta)
+    val carta2 = deckController.drawCard()
+    println(carta2)
 }
